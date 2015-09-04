0997a73d02cd5c40a99fedc79134f2ab4fd93e8a
qiqi8226 <qiqi8226@gmail.com>
2015-09-04 10:49:17 +0800

    modify the default lang

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 1234,
     root_path: "/Users/qiqi",
-    title: "Android Advanced ",
+    title: "Android Distribution System",
     logo_link: "http://img1.guixue.com/v/static/gx2014/images/public/logo.png"
 }
